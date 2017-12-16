b09c02bf7502df04029b17536aecb39e5c04de3b
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-16 20:48:35 +0000

    Update phptests.sh

--- php_tests/phptests.sh
+++ php_tests/phptests.sh
@@ -3,18 +3,48 @@ dir="/$USER/Desktop/login-system/php_tests";
 cd "$dir";
 
 # todo getchilditem function and for loop
-function getchilditems()
+function getchilditem()
 {
   obj="$1";
-  
+  ls $obj;
 }
 
-$scripts=$(getchilditems "$path");
+function runphp()
+{
+	script="$1";
+	clear;
+	echo -e "\nphp script: $script\n";
+	cat "$script";
+	echo -e "\n";
+	read
 
-for each in $scripts; do
-  php "$script"
-done  
+	php "$script" ;
+
+	read
+	clear ;
+}
+
+
+function loop()
+{
+
+parent="/root/Desktop/login-system/php_tests";
+myarray=$(getchilditem "$parent");
+cd "$parent";
+
+
+
+
+for each in $myarray; do
+	#echo -e "script: $each";
+	runphp "$each";
+done		
+}
+
+
+
+
   
 
 php args.php "Nicholas" "Jacquet";
-php variables.php;
+
