a97b1ed8d76af0a2a1a461e4a229680e26e7e089
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-16 21:03:44 +0000

    Create function.php

---
+++ php_pages/function.php
@@ -0,0 +1,21 @@
+<!-- https://www.tutorialspoint.com/php/php_functions.htm -->
+
+<html>
+   
+   <head>
+      <title>Writing PHP Function with Parameters</title>
+   </head>
+   
+   <body>
+   
+      <?php
+         function addFunction($num1, $num2) {
+            $sum = $num1 + $num2;
+            echo "Sum of the two numbers is : $sum";
+         }
+         
+         addFunction(10, 20);
+      ?>
+      
+   </body>
+</html>
