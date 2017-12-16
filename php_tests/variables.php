46acc938fb2e0571d6f5faf6d0d38e6a093fb4dc
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-16 19:58:11 +0000

    Create variables.php

---
+++ php_tests/variables.php
@@ -0,0 +1,12 @@
+<?php
+// topic: PHP 5 variables	
+//   url: https://www.w3schools.com/PhP/php_variables.asp
+
+	$txt = "Hello world!";
+	$x = 5;
+	$y = 10.5;
+
+	echo "$txt\n";
+	echo "$x\n";
+	echo "$y\n";
+?> 
