cd3a34c721c672508c3deee02f28d22242c53baa
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-16 20:20:49 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,6 +7,13 @@ git clone https://github.com/InspectorChocolatey/login-system login-system;
 
 git clone https://github.com/InspectorChocolatey/php php;
 
+
+# install sublime text editor
+sub="/root/Desktop/php/phpmyadmin/sublime/sublime.sh";
+chmod u+x "$sub" -v;
+$sub
+
+
 # clone php
 # download sublime
 # setup database
