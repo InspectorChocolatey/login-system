94d9741373d3ba80c8f004b169fb0c7ca936b2f1
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-17 00:43:53 +0000

    Create countvowels.php

---
+++ php_tests/strings/countvowels.php
@@ -0,0 +1,22 @@
+<?php
+/* 
+	php cookbook (p8)
+*/
+
+$string = "This weekend, I'm going shopping for a pet chicken.";
+
+$vowels = 0;
+
+for($i = 0, $j = strlen($string); $i < $j; $i++) {
+
+	if(strstr('aeiouAEIOU', $string[$i])) {
+
+		$vowels++;
+
+	}
+
+}
+
+echo "$vowels\n";
+
+?>
