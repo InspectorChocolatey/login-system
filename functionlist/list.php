46d47b116f3a157f6e2222b23a9cc1ff2d3c9480
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-17 00:23:35 +0000

    Update list.php

--- functionlist/list.php
+++ functionlist/list.php
@@ -1,9 +1,13 @@
 <?
 
+include();
 echo "";
 empty();
 time();
 date();
 strtotime();
-
+mysqli_connect("127.0.0.1", $uid, $pwd, "logindb" );
+mysqli_connect_error()
+mysqli_fetch_row($result);
+die("Connection failed: ". mysqli_connect_error());
 ?>
