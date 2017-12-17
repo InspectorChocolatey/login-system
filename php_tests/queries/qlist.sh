5eb08527314303a4af027d6ee115ba416f03f229
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-17 05:50:50 +0000

    Update qlist.sh

--- php_tests/queries/qlist.sh
+++ php_tests/queries/qlist.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-q='INSERT into user (first, last, uid, pwd) values ("Donald","Trump", "trump4prez", "fuckU");';
-php "/$USER/Desktop/login-system/php_tests/queries/query.php" "$q";
+a='INSERT into user (first, last, uid, pwd) values ("Donald","Trump", "trump4prez", "fuckU");';
+b='INSERT into user (first, last, uid, pwd) values ("Barrack","Obama", "nig", "nog");';
+php "/$USER/Desktop/login-system/php_tests/queries/query.php" "$a";
+php "/$USER/Desktop/login-system/php_tests/queries/query.php" "$b";
