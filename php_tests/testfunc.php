31570e9576e33bc289967dae330df4c5a7c2e104
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-17 00:14:34 +0000

    Create testfunc.php

---
+++ php_tests/testfunc.php
@@ -0,0 +1,23 @@
+<?php
+$mystr = '';
+
+if(empty($mystr)) {
+
+	echo "My string string is empty\n";
+
+} else {
+
+	echo "My string is not empty...\n";
+
+}
+
+$nextWeek = time() + (7 * 24 * 60 * 60);
+
+echo 'Now:       '. date('Y-m-d') ."\n";
+echo 'Next Week: '. date('Y-m-d', $nextWeek) ."\n";
+// or using strtotime():
+echo 'Next Week: '. date('Y-m-d', strtotime('+1 week')) ."\n";
+
+time();
+
+?>
