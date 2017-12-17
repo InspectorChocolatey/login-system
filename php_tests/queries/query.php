dbda012bd995cd45d99575d314c60af5440b7e5a
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-17 05:36:09 +0000

    Create query.php

---
+++ php_tests/queries/query.php
@@ -0,0 +1,34 @@
+<?php
+
+$sql = $argv[1];
+#echo "$str\n";
+
+$db = "logindb"
+
+$uid = "phpmyadmin";
+$pwd = "password";
+
+$conn = mysqli_connect("127.0.0.1", $uid, $pwd, $db );
+
+if(!$conn) {
+	die("Connection failed: ". mysqli_connect_error());
+} else {
+	#$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
+	#$sql = "SELECT * FROM user ;";
+	$result = mysqli_query($conn, $sql);
+	
+	
+if(!$result) {
+
+      echo "Could not connect to database: $db";
+
+} else {
+
+
+		echo "Query executed: $sql";
+
+		//$_SESSION['id'] = $row['id'];
+	}
+}
+
+?>
