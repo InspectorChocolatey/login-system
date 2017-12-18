194f6f189d4cda1ff7ca1aa199d7373608909bbb
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2017-12-18 21:11:01 +0000

    Update permissions.sh

--- upload_download/permissions.sh
+++ upload_download/permissions.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
 # https://stackoverflow.com/questions/14449731/uploading-file-via-php-doesnt-work
-$webroot="/var/www/html";
+webroot="/var/www/html";
 cd "$webroot";
 chmod 777 "uploads" -v;
