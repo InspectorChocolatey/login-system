ec0bdc2b35cccc249f1c8505c0a2f2f3e0246af8
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2020-05-17 13:17:42 -0500

    Update function.php

--- php_pages/function.php
+++ php_pages/function.php
@@ -1,21 +1,15 @@
-<!-- https://www.tutorialspoint.com/php/php_functions.htm -->
+<?php
+	/**
+	* @Description: Writing PHP Function with Parameters
+	* @References:
+	*    https://www.tutorialspoint.com/php/php_functions.htm
+	*
+	*/
+	function addFunction($num1, $num2) 
+	{
+		$sum = $num1 + $num2;
+		echo "Sum of the two numbers is : $sum";
+	}
 
-<html>
-   
-   <head>
-      <title>Writing PHP Function with Parameters</title>
-   </head>
-   
-   <body>
-   
-      <?php
-         function addFunction($num1, $num2) {
-            $sum = $num1 + $num2;
-            echo "Sum of the two numbers is : $sum";
-         }
-         
-         addFunction(10, 20);
-      ?>
-      
-   </body>
-</html>
+	addFunction(10, 20);
+?>
